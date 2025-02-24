32475755d24c2495f7c2b0aaf746fa6e20260779
LorenoBiffi <loreno.biffi@gmail.com>
2025-02-24 21:44:46 +0100

    Erstes Commit: Projekt initialisiert

---
+++ vue-project/src/App.vue
@@ -0,0 +1,11 @@
+<script setup>
+import { ref } from 'vue';
+
+const showText = ref(true);
+</script>
+
+<template>
+  <button @click="showText = !showText">Umschalten</button>
+  <p v-if="showText">Ich bin sichtbar!</p>
+  <p v-else>Ich bin versteckt!</p>
+</template>
---
+++ vue-project/src/components/HelloWorld.vue
@@ -0,0 +1,13 @@
+<script setup>
+const message = "Hallo, Vue!";
+</script>
+
+<template>
+  <h1>{{ message }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: #42b983;
+}
+</style>
